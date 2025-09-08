fe6e5416e2e1bd11f50492c1b09b515bb84f41c8
Dmitrii Liashko <dmitrii.liashko>
2025-09-08 21:50:32 +0300

    Задание 3 - Получение 20 натуральных нечётных чисел разными способами (4 способа)

--- lw1/task3.hs
+++ lw1/task3.hs
@@ -6,7 +6,18 @@ getOddIntList :: [Integer] -> [Integer]
 getOddIntList  = filter odd
 
 main = do
-  let result = getOddIntList naturalsInt
-  print ("3.1: " ++ show result)
-  print ("Length of list [3.1]: " ++ show (length result))
+  let result1 = getOddIntList naturalsInt
+  print ("3.1: " ++ show result1)
+  print ("Length of list [3.1]: " ++ show (length result1))
 
+  let result2 = take 20 (filter odd [1..])
+  print ("3.2: " ++ show result2)
+  print ("Length of list [3.2]: " ++ show (length result2))
+
+  let result3 = take 20 (iterate (+2) 1)
+  print ("3.3: " ++ show result3)
+  print ("Length of list [3.3]: " ++ show (length result3))
+
+  let result4 = take 20 [1, 3..]
+  print ("3.4: " ++ show result4)
+  print ("Length of list [3.4]: " ++ show (length result4))
