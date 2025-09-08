2d0645c65fd56dfbb6ad2edf9a0be1a212f9b97b
Dmitrii Liashko <dmitrii.liashko>
2025-09-08 21:00:32 +0300

    Первый вариант реализации 2 задачи

--- lw1/task2.hs
+++ lw1/task2.hs
@@ -1,8 +1,32 @@
 firstList :: [Char]
 firstList = ['a', 'b', 'c']
 
+secondList :: [[Char]]
+secondList =  [['a', 'b'], ['c','d']]
+
+thirdList :: [[Char]]
+thirdList =  [['a', 'c', 'd'], ['a','b']]
+
+fourthList :: [[Char]]
+fourthList = [['a','d'], ['b', 'c']]
+
+
 getElementFromFirstList :: [a] -> a
 getElementFromFirstList e = head(tail e)
 
+
+getElementFromSecondList :: [[a]] -> a
+getElementFromSecondList e = head(tail(head e))
+
+getElementFromThirdList :: [[a]] -> a
+getElementFromThirdList e = head(tail(head(tail e)))
+
+getElementFromFourthList :: [[a]] -> a
+getElementFromFourthList e = head(head(tail e))
+
+
 main = do
-  print (getElementFromFirstList firstList)
+  print ("2.1: " ++ [getElementFromFirstList firstList])
+  print ("2.2: " ++ [getElementFromSecondList secondList])
+  print ("2.3: " ++ [getElementFromThirdList thirdList])
+  print ("2.4: " ++ [getElementFromFourthList fourthList])
