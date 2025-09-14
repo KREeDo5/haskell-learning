540dca875d24d885774b0e6503ea576296a984ec
Dmitrii Liashko <dmitrii.liashko>
2025-09-14 15:07:41 +0300

    Задание 3

--- lw2/task3.hs
+++ lw2/task3.hs
@@ -1,2 +1,12 @@
+listSumm :: [Int] -> [Int] -> [Int]
+listSumm l1 l2
+    | null l1 && null l2 = []
+    | null l2 = l1
+    | null l1 = l2
+    | otherwise = (head l1 + head l2) : listSumm (tail l1) (tail l2)
+main :: IO ()
 main = do
-    print ("2.3")
+    print (listSumm [1, 2, 3] [4, 3, 2])
+    print (listSumm [1] [4, 5, 6 - 1])
+    print (listSumm [] [])
+    print (listSumm [12] [])
