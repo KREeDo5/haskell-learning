48d11f4d9b02d9127810040a47f996bc957a6885
Dmitrii Liashko <dmitrii.liashko>
2025-09-14 14:36:39 +0300

    Задание 2

--- lw2/task2.hs
+++ lw2/task2.hs
@@ -1,2 +1,6 @@
+insert :: [a] -> a -> Int -> [a]
+insert list el n = take n list ++ [el] ++ drop n list
+
 main = do
-    print ("2.2")
+    print (insert [] 52 (-2))
+    print (insert [1, 2, 3, 4] 52 2)
